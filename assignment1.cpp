5059f14ab944e8f146886b2711f6d1b3bb2f8f66
Sahil Merchant <sahilmerchant14@gmail.com>
2024-01-13 11:12:48 -0500

    Added cpp assignment 1

---
+++ assignment1.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void pattern1_printer(int num)
+{
+    for(int i = 1; i <= num; i++)
+    {
+        for(int j = 1; j <= i; j++)
+        {
+            printf("* ");
+        }
+
+        printf("\n");
+    }
+}
+
+void pattern2_printer(int num)
+{
+    for(int i = 1; i <= num; i++)
+    {
+        for(int j = num; j >= i; j--)
+        {
+            printf("* ");
+        }
+
+        printf("\n");
+    }
+
+}
+
+
+int main()
+{
+    //pattern1_printer(5);
+    pattern2_printer(5);
+}
